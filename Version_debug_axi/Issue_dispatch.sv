9a8da122825fa3fae5f2e1c2577b0d959059cbf6
Co336 <cw1@mail.ustc.edu.cn>
2024-07-24 16:49:56 +0800

    add version_axi_debug

---
+++ Version_debug_axi/Issue_dispatch.sv
@@ -0,0 +1,194 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/07/17 13:25:11
+// Design Name: 
+// Module Name: Issue_dispatch
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+import Public_Info::*;
+module Issue_dispatch(
+    input [ 0: 0] clk,
+    input PC_set i_set1,
+    input PC_set i_set2,
+    input [ 1: 0] i_is_valid,
+
+
+    output PC_set o_set1,
+    output PC_set o_set2,
+    output logic [ 1: 0] o_usingNUM
+
+    );
+
+    logic [ 0: 0] flush;
+    logic [ 0: 0] stall;
+
+    logic [ 0: 0] last_ld;
+    logic [ 4: 0] rf_waddr;
+
+    logic [ 0: 0] double_BR;
+    logic [ 0: 0] double_LDSW;
+    logic [ 0: 0] LDSW_Mul_Div_plus_any;
+    // logic [ 0: 0] any_plus_LDSW;
+    logic [ 0: 0] RAW_exist;
+    logic [ 1: 0] ld_exist;
+    logic [ 0: 0] lock_in_1;
+    logic [ 0: 0] lock_in_2;
+
+
+
+
+    logic [ 1: 0] is_valid;
+    assign is_valid = {i_set1.o_valid & i_is_valid[1], i_set2.o_valid & i_is_valid[0]};
+
+
+    assign o_set1.instruction   = i_set1.instruction  ;
+    assign o_set1.PC            = i_set1.PC           ;
+    // assign o_set1.        = i_set1.o_valid      ;
+    assign o_set1.inst_type     = i_set1.inst_type    ;
+    assign o_set1.br_type       = i_set1.br_type      ;
+    assign o_set1.imm           = i_set1.imm          ;
+    assign o_set1.rf_rd         = i_set1.rf_rd        ;
+    assign o_set1.rf_we         = i_set1.rf_we        ;
+    assign o_set1.alu_src1_sel  = i_set1.alu_src1_sel ;
+    assign o_set1.alu_src2_sel  = i_set1.alu_src2_sel ;
+    assign o_set1.alu_op        = i_set1.alu_op       ;
+    assign o_set1.mem_we        = i_set1.mem_we       ;
+    assign o_set1.ldst_type     = i_set1.ldst_type    ;
+    assign o_set1.wb_sel        = i_set1.wb_sel       ;
+    assign o_set1.mux_sel       = i_set1.mux_sel      ;
+    assign o_set1.rf_raddr1     = i_set1.rf_raddr1    ;
+    assign o_set1.rf_raddr2     = i_set1.rf_raddr2    ;
+    assign o_set1.rf_rdata1     = i_set1.rf_rdata1    ;
+    assign o_set1.rf_rdata2     = i_set1.rf_rdata2    ;
+    assign o_set1.sign_bit      = i_set1.sign_bit     ;
+
+
+    assign o_set2.instruction   = i_set2.instruction  ;
+    assign o_set2.PC            = i_set2.PC           ;
+    // assign o_set2.o_valid       = i_set2.o_valid      ;
+    assign o_set2.inst_type     = i_set2.inst_type    ;
+    assign o_set2.br_type       = i_set2.br_type      ;
+    assign o_set2.imm           = i_set2.imm          ;
+    assign o_set2.rf_rd         = i_set2.rf_rd        ;
+    assign o_set2.rf_we         = i_set2.rf_we        ;
+    assign o_set2.alu_src1_sel  = i_set2.alu_src1_sel ;
+    assign o_set2.alu_src2_sel  = i_set2.alu_src2_sel ;
+    assign o_set2.alu_op        = i_set2.alu_op       ;
+    assign o_set2.mem_we        = i_set2.mem_we       ;
+    assign o_set2.ldst_type     = i_set2.ldst_type    ;
+    assign o_set2.wb_sel        = i_set2.wb_sel       ;
+    assign o_set2.mux_sel       = i_set2.mux_sel      ;
+    assign o_set2.rf_raddr1     = i_set2.rf_raddr1    ;
+    assign o_set2.rf_raddr2     = i_set2.rf_raddr2    ;
+    assign o_set2.rf_rdata1     = i_set2.rf_rdata1    ;
+    assign o_set2.rf_rdata2     = i_set2.rf_rdata2    ;
+    assign o_set2.sign_bit      = i_set2.sign_bit     ;
+
+    // 对于处在前面A位置，但是在B位置发射的指令，dispatch位置不变，在Issue_EXE根据选择信号抉择
+    always @(*) begin
+        if(flush || stall) begin
+            o_set1.o_valid = 1'b0;
+            o_set2.o_valid = 1'b0;
+            o_usingNUM = 2'b00;
+        end
+        else begin
+            case (is_valid)
+                2'b00: begin
+                    o_set1.o_valid = 1'b0;
+                    o_set2.o_valid = 1'b0;
+                    o_usingNUM = 2'b00;
+                end
+                2'b10: begin
+                    if( !lock_in_1) begin
+                        o_set1.o_valid = 1'b1;
+                        o_set2.o_valid = 1'b0;
+                        o_usingNUM = 2'd1;
+                    end
+                    else begin
+                        o_set1.o_valid = 1'b0;
+                        o_set2.o_valid = 1'b0;
+                        o_usingNUM = 2'b00;
+                    end
+                end
+                2'b01: begin
+                    o_set1.o_valid = 1'b0;
+                    o_set2.o_valid = 1'b0;
+                    o_usingNUM = 2'b00;
+                end
+                2'b11: begin
+                    // 两个合法，开始检测
+                    
+                    if( lock_in_1 ) begin
+                        o_set1.o_valid = 1'b0;
+                        o_set2.o_valid = 1'b0;
+                        o_usingNUM = 2'b00;
+                    end
+                    else begin
+                        if( lock_in_2 ) begin
+                            o_set1.o_valid = 1'b1;
+                            o_set2.o_valid = 1'b0;
+                            o_usingNUM = 2'd1;
+                        end
+                        else begin
+                            // 都没有互锁现象
+                            if(LDSW_Mul_Div_plus_any) begin
+                                // 第一个指令只能发射在B位置
+                                o_set1.o_valid = 1'b1;
+                                o_set2.o_valid = 1'b0;
+                                o_usingNUM = 2'd1;
+                            end
+                            else if(double_BR || RAW_exist) begin
+                                o_set1.o_valid = 1'b1;
+                                o_set2.o_valid = 1'b0;
+                                o_usingNUM = 2'd1;
+                            end
+                            // TODO: 也许可以合并
+                            else begin
+                                o_set1.o_valid = 1'b1;
+                                o_set2.o_valid = 1'b1;
+                                o_usingNUM = 2'd2;
+                            end
+                        end
+                    end
+                end
+            endcase
+        end
+    end
+    // assign ld_exist = (LDSW_Mul_Div_plus_any & (o_usingNUM != 2'b00)) | (any_plus_LDSW & (o_usingNUM == 2'd2)) | ((is_valid == 2'b10) & (o_usingNUM != 2'b00) & ~o_set1.ldst_type[3]);
+    assign ld_exist[1] = (o_usingNUM != 2'd0) & ~o_set1.ldst_type[3];
+    assign ld_exist[0] = (o_usingNUM == 2'd2) & ~o_set2.ldst_type[3];
+
+
+    assign double_BR     = (is_valid == 2'b11) ? ( (|(i_set1.br_type)) && (|(i_set2.br_type))) : 0; // 同时为BR，两个都不是全0
+    assign LDSW_Mul_Div_plus_any = (is_valid == 2'b11) ? (i_set1.inst_type != 10'h001) : 0; // 前LDSW，Div， Mul，后任意
+    
+
+    assign double_LDSW   = (is_valid == 2'b11) ? ( ~((i_set1.ldst_type[3]) | (i_set2.ldst_type[3])) ) : 0; // 同时为LDSW，最高位都是0
+    assign RAW_exist     = (is_valid == 2'b11 && i_set1.rf_rd != 0) ? ( (i_set1.rf_rd == i_set2.rf_raddr1) || (i_set1.rf_rd == i_set2.rf_raddr2) ) : 0; // 前rd=后rf,且rd = 0
+
+    assign lock_in_1     = (is_valid[1] & last_ld) ? (rf_waddr == i_set1.rf_raddr1) || (rf_waddr == i_set1.rf_raddr2) : 0; // LW与A指令互锁
+    assign lock_in_2     = (is_valid[0] & last_ld) ? (rf_waddr == i_set2.rf_raddr1) || (rf_waddr == i_set2.rf_raddr2) : 0; // LW与B指令互锁
+
+
+    always @(posedge clk) begin
+        last_ld <= ld_exist;
+        rf_waddr <= (ld_exist[1]) ? i_set1.rf_rd : i_set2.rf_rd;
+    end
+
+
+
+endmodule
---
+++ Version_debug_axi/Pipeline_Register.sv
@@ -0,0 +1,122 @@
+module Pipeline_Register(
+    input                       clk,
+    input                       rstn,
+    input                       stall_dcache,
+    input                       stall_div,
+    
+    input           [31: 0]     EX_pc_a,            //A指令的PC
+    input           [31: 0]     EX_pc_b,            //B指令的PC
+    output  reg     [31: 0]     WB_pc_a,            //A指令的PC
+    output  reg     [31: 0]     WB_pc_b,            //B指令的PC
+    input                       EX_br_a,            //A指令是否需要修正预测的结果，在EX段发生跳转
+    input           [31: 0]     EX_alu_result_a,    //A指令的运算结果
+    input           [31: 0]     EX_alu_result_b,    //B指令的运算结果
+    output  reg     [31: 0]     MEM_alu_result_a,
+    output  reg     [31: 0]     MEM_alu_result_b,
+    output  reg     [31: 0]     WB_alu_result_a,
+    output  reg     [31: 0]     WB_alu_result_b,
+
+    input           [63: 0]     EX_mul_tmp1,        //乘法器临时结果1
+    input           [63: 0]     EX_mul_tmp2,        //乘法器临时结果2
+    output  reg     [63: 0]     MEM_mul_tmp1,
+    output  reg     [63: 0]     MEM_mul_tmp2,
+
+    input                       EX_rf_we_a,         //A指令寄存器写使能
+    input                       EX_rf_we_b,         //B指令寄存器写使能
+    input           [ 4: 0]     EX_rf_waddr_a,      //A指令寄存器写地址
+    input           [ 4: 0]     EX_rf_waddr_b,      //B指令寄存器写地址
+    input           [ 5: 0]     EX_wb_mux_select_b,
+    output  reg     [ 5: 0]     MEM_wb_mux_select_b,
+    input           [ 2: 0]     EX_mem_type_a,      //A指令访存类型
+    input           [ 2: 0]     EX_mem_type_b,      //B指令访存类型
+
+    output  reg                 MEM_rf_we_a,
+    output  reg                 MEM_rf_we_b,
+    output  reg     [ 4: 0]     MEM_rf_waddr_a,
+    output  reg     [ 4: 0]     MEM_rf_waddr_b,
+    input           [31: 0]     MEM_rf_wdata_a,
+    input           [31: 0]     MEM_rf_wdata_b,
+    output  reg     [ 2: 0]     MEM_mem_type_a,
+    output  reg     [ 2: 0]     MEM_mem_type_b,
+
+    output  reg                 WB_rf_we_a,
+    output  reg                 WB_rf_we_b,
+    output  reg     [ 4: 0]     WB_rf_waddr_a,
+    output  reg     [ 4: 0]     WB_rf_waddr_b,
+    output  reg     [31: 0]     WB_rf_wdata_a,
+    output  reg     [31: 0]     WB_rf_wdata_b
+);
+logic   [31: 0]     MEM_pc_a;
+logic   [31: 0]     MEM_pc_b;
+always@(posedge clk,negedge rstn)
+begin
+    if(!rstn)
+    begin
+        MEM_alu_result_a<=32'h0000_0000;
+        MEM_alu_result_b<=32'h0000_0000;
+        WB_alu_result_a<=32'h0000_0000;
+        WB_alu_result_b<=32'h0000_0000;
+        MEM_rf_we_a<=1'b0;
+        MEM_rf_we_b<=1'b0;
+        MEM_rf_waddr_a<=5'b00000;
+        MEM_rf_waddr_b<=5'b00000;
+        MEM_mem_type_a<=3'b000;
+        MEM_mem_type_b<=3'b000;
+        WB_rf_we_a<=1'b0;
+        WB_rf_we_b<=1'b0;
+        WB_rf_waddr_a<=5'b00000;
+        WB_rf_waddr_b<=5'b00000;
+        WB_rf_wdata_a<=32'h0000_0000;
+        WB_rf_wdata_b<=32'h0000_0000;
+        MEM_wb_mux_select_b<=6'b000000;
+        MEM_mul_tmp1<=64'h0000_0000;
+        MEM_mul_tmp2<=64'h0000_0000;
+        MEM_pc_a<=32'h0000_0000;
+        MEM_pc_b<=32'h0000_0000;
+        WB_pc_a<=32'h0000_0000;
+        WB_pc_b<=32'h0000_0000;
+    end
+    else if(!stall_dcache&&!stall_div)begin //考虑到前递，stall_dcache应阻塞所有段间寄存器
+        //EX->MEM
+        //不需要修正分支预测
+        if(!EX_br_a) begin 
+            MEM_alu_result_b<=EX_alu_result_b;
+            MEM_rf_we_b<=EX_rf_we_b;
+            MEM_wb_mux_select_b<=EX_wb_mux_select_b;
+            MEM_mul_tmp1<=EX_mul_tmp1;
+            MEM_mul_tmp2<=EX_mul_tmp2;
+            MEM_pc_b<=EX_pc_b;
+        end
+        //需要修正分支预测
+        else begin 
+            MEM_alu_result_b<=32'h0000_0000;
+            MEM_rf_we_b<=1'b0;
+            MEM_wb_mux_select_b<=6'b000000;
+            MEM_mul_tmp1<=64'h0000_0000;
+            MEM_mul_tmp2<=64'h0000_0000;
+            MEM_pc_b<=32'h0000_0000;
+        end 
+        MEM_alu_result_a<=EX_alu_result_a;
+        MEM_rf_we_a<=EX_rf_we_a;
+        MEM_rf_waddr_a<=EX_rf_waddr_a;
+        MEM_rf_waddr_b<=EX_rf_waddr_b;
+        MEM_mem_type_a<=EX_mem_type_a;
+        MEM_mem_type_b<=EX_mem_type_b;
+        MEM_pc_a<=EX_pc_a;
+
+        //MEM->WB
+        WB_alu_result_a<=MEM_alu_result_a;
+        WB_alu_result_b<=MEM_alu_result_b;
+        WB_rf_we_a<=MEM_rf_we_a;
+        WB_rf_we_b<=MEM_rf_we_b;
+        WB_rf_waddr_a<=MEM_rf_waddr_a;
+        WB_rf_waddr_b<=MEM_rf_waddr_b;
+        WB_rf_wdata_a<=MEM_rf_wdata_a;
+        WB_rf_wdata_b<=MEM_rf_wdata_b;
+        WB_pc_a<=MEM_pc_a;
+        WB_pc_b<=MEM_pc_b;
+    end
+    else begin end
+end
+
+endmodule
