e58a53306b110d9d33da71303d3a88bf109275d4
Yang Yibo <yangyibo@mail.ustc.edu.cn>
2024-07-29 21:08:24 +0800

    CSR interrupt; 2-Read; Timer

--- YangYibo/CSR.sv
+++ YangYibo/CSR.sv
@@ -1,13 +1,16 @@
 `include "CSR_h.vh"
 module CSR(
     input                           clk,
+    input                           stable_clk,
     input                           rstn,
     //software query port (exe stage)
     input                           software_we,//是否允许软件写入
-    input           [13:0]          raddr,
-    output reg      [31:0]          rdata,  //写优先
+    input           [13:0]          raddr_a,    //允许同时读两个地址的数据
+    input           [13:0]          raddr_b,
+    output reg      [31:0]          rdata_a,    //读优先
+    output reg      [31:0]          rdata_b,    //读优先
     input           [13:0]          waddr,
-    input           [31:0]          we,
+    input           [31:0]          we,         //只能同时写一个地址的数据
     input           [31:0]          wdata,
 
     //current machine state
@@ -70,14 +73,14 @@ reg [`ESTAT_IS_0] estat_is_0;
 reg [`ESTAT_ECODE] estat_ecode;
 reg [`ESTAT_ESUBCODE] estat_subecode;
 wire [31:0] csr_estat;
-assign csr_estat[`ESTAT_IS_0] = estat_is_0;
 //龙芯架构32位精简版参考手册 v1.0 p.59 只提到“1个核间中断（IPI），
 //1个定时器中断（TI）,8个硬中断（HWI0~HWI7）”
-//询问该公司的技术人员后，我们得知
-//is[9:2] = hw[7:0] 
-//is[10] 是 la64的特有中断，在la32r中恒为0
-//is[11] = TI
-//is[12] = IPI
+//is[1:0]      软件中断
+//is[9:2]= HWI[7:0] 硬中断HWI 
+//is[10] = 0   la32r中恒为0
+//is[11] = TI  定时器中断
+//is[12] = IPI 核间中断
+assign csr_estat[`ESTAT_IS_0] = estat_is_0;
 assign csr_estat[`ESTAT_IS_1] = {1'b0,timer_int,1'b0,hardware_int};
 assign csr_estat[`ESTAT_ZERO_0] = 0;
 assign csr_estat[`ESTAT_ECODE]  = estat_ecode;
@@ -94,6 +97,23 @@ assign csr_eentry[`EENTRY_ZERO] = 0;
 assign csr_eentry[`EENTRY_VA]   = eentry_va;
 //SAVE0~3
 reg [31:0] csr_save0,csr_save1,csr_save2,csr_save3;
+
+//TID
+reg [31:0] csr_tid;
+//TCFG
+reg [`TCFG_EN] tcfg_en;
+reg [`TCFG_PERIODIC] tcfg_periodic;
+reg [`TCFG_INITVAL] tcfg_initval;
+wire [31:0] csr_tcfg;
+assign csr_tcfg[`TCFG_EN]   = tcfg_en;
+assign csr_tcfg[`TCFG_PERIODIC] = tcfg_periodic;
+assign csr_tcfg[`TCFG_INITVAL]  = tcfg_initval;
+//TVAL
+reg [31:0] csr_tval;
+//TICLR
+wire[31:0] csr_ticlr = 0;
+//CTAG
+reg [31:0] csr_ctag;
 ///////////////////////////////////////
 wire [1:0] pg_da_next = {we[4]?wdata[4]:crmd_pg,we[3]?wdata[3]:crmd_da};
 ///////////////////////////////////////
@@ -167,7 +187,7 @@ always @(posedge clk)
         estat_subecode <= 0;
     end else if(ecode_we) begin
         estat_ecode <= ecode_in[5:0];
-        estat_subecode <= ecode_in[5:0]==0 ? 0:{8'b0,ecode_in[6]};
+        estat_subecode <= (|ecode_in[5:0]) ? {8'b0,ecode_in[6]}:9'b0;   //存在1（非中断例外）? 二级编码 : 0
     end else if(software_we&&waddr==`CSR_ESTAT) begin
         if(we[0]) estat_is_0[0]<=wdata[0];
         if(we[1]) estat_is_0[1]<=wdata[1];
@@ -436,34 +456,203 @@ always @(posedge clk)
         if(we[30]) csr_save3[30]<=wdata[30];
         if(we[31]) csr_save3[31]<=wdata[31];
     end
+//TID
+always @(posedge clk)
+    if(~rstn) begin
+        csr_tid <= 0;
+    end else if(software_we&&waddr==`CSR_TID) begin
+        if(we[ 0]) csr_tid[ 0]<=wdata[ 0];
+        if(we[ 1]) csr_tid[ 1]<=wdata[ 1];
+        if(we[ 2]) csr_tid[ 2]<=wdata[ 2];
+        if(we[ 3]) csr_tid[ 3]<=wdata[ 3];
+        if(we[ 4]) csr_tid[ 4]<=wdata[ 4];
+        if(we[ 5]) csr_tid[ 5]<=wdata[ 5];
+        if(we[ 6]) csr_tid[ 6]<=wdata[ 6];
+        if(we[ 7]) csr_tid[ 7]<=wdata[ 7];
+        if(we[ 8]) csr_tid[ 8]<=wdata[ 8];
+        if(we[ 9]) csr_tid[ 9]<=wdata[ 9];
+        if(we[10]) csr_tid[10]<=wdata[10];
+        if(we[11]) csr_tid[11]<=wdata[11];
+        if(we[12]) csr_tid[12]<=wdata[12];
+        if(we[13]) csr_tid[13]<=wdata[13];
+        if(we[14]) csr_tid[14]<=wdata[14];
+        if(we[15]) csr_tid[15]<=wdata[15];
+        if(we[16]) csr_tid[16]<=wdata[16];
+        if(we[17]) csr_tid[17]<=wdata[17];
+        if(we[18]) csr_tid[18]<=wdata[18];
+        if(we[19]) csr_tid[19]<=wdata[19];
+        if(we[20]) csr_tid[20]<=wdata[20];
+        if(we[21]) csr_tid[21]<=wdata[21];
+        if(we[22]) csr_tid[22]<=wdata[22];
+        if(we[23]) csr_tid[23]<=wdata[23];
+        if(we[24]) csr_tid[24]<=wdata[24];
+        if(we[25]) csr_tid[25]<=wdata[25];
+        if(we[26]) csr_tid[26]<=wdata[26];
+        if(we[27]) csr_tid[27]<=wdata[27];
+        if(we[28]) csr_tid[28]<=wdata[28];
+        if(we[29]) csr_tid[29]<=wdata[29];
+        if(we[30]) csr_tid[30]<=wdata[30];
+        if(we[31]) csr_tid[31]<=wdata[31];
+    end
+
+//TCFG
+always @(posedge clk)
+    if(~rstn) begin
+        tcfg_en <= 0;
+        tcfg_initval <= 0;
+        tcfg_periodic<=0;
+    end else if(software_we&&waddr==`CSR_TCFG) begin
+        if(we[ 0]) tcfg_en[ 0]     <=wdata[ 0];
+        if(we[ 1]) tcfg_periodic[1]<=wdata[ 1];
+        if(we[ 2]) tcfg_initval[ 2]<=wdata[ 2];
+        if(we[ 3]) tcfg_initval[ 3]<=wdata[ 3];
+        if(we[ 4]) tcfg_initval[ 4]<=wdata[ 4];
+        if(we[ 5]) tcfg_initval[ 5]<=wdata[ 5];
+        if(we[ 6]) tcfg_initval[ 6]<=wdata[ 6];
+        if(we[ 7]) tcfg_initval[ 7]<=wdata[ 7];
+        if(we[ 8]) tcfg_initval[ 8]<=wdata[ 8];
+        if(we[ 9]) tcfg_initval[ 9]<=wdata[ 9];
+        if(we[10]) tcfg_initval[10]<=wdata[10];
+        if(we[11]) tcfg_initval[11]<=wdata[11];
+        if(we[12]) tcfg_initval[12]<=wdata[12];
+        if(we[13]) tcfg_initval[13]<=wdata[13];
+        if(we[14]) tcfg_initval[14]<=wdata[14];
+        if(we[15]) tcfg_initval[15]<=wdata[15];
+        if(we[16]) tcfg_initval[16]<=wdata[16];
+        if(we[17]) tcfg_initval[17]<=wdata[17];
+        if(we[18]) tcfg_initval[18]<=wdata[18];
+        if(we[19]) tcfg_initval[19]<=wdata[19];
+        if(we[20]) tcfg_initval[20]<=wdata[20];
+        if(we[21]) tcfg_initval[21]<=wdata[21];
+        if(we[22]) tcfg_initval[22]<=wdata[22];
+        if(we[23]) tcfg_initval[23]<=wdata[23];
+        if(we[24]) tcfg_initval[24]<=wdata[24];
+        if(we[25]) tcfg_initval[25]<=wdata[25];
+        if(we[26]) tcfg_initval[26]<=wdata[26];
+        if(we[27]) tcfg_initval[27]<=wdata[27];
+        if(we[28]) tcfg_initval[28]<=wdata[28];
+        if(we[29]) tcfg_initval[29]<=wdata[29];
+        if(we[30]) tcfg_initval[30]<=wdata[30];
+        if(we[31]) tcfg_initval[31]<=wdata[31];
+    end
+
+reg just_set_timer;
+always @(posedge clk)
+    if(~rstn) just_set_timer<=0;
+    else if(software_we && waddr==`CSR_TCFG && we!=0)
+        just_set_timer<=1;
+    else just_set_timer<=0;
+
+//TVAL
+reg time_out;
+always @(posedge stable_clk)
+    if(~rstn) begin
+        csr_tval <= 0;
+        time_out <= 0;
+    end
+    //FIXME: 设置TCFG.InitVal后自动重置定时器，这在手册中未提及
+    else if(csr_tval==0||just_set_timer) begin
+        time_out <= 0;
+        //计时器的初始值比标准大1，否则给定时器设置0无法触发中断
+        if(tcfg_periodic||just_set_timer) csr_tval<={tcfg_initval[`TCFG_INITVAL],2'd1};
+    end else if(tcfg_en) begin
+        csr_tval<=csr_tval-1;
+        time_out<=csr_tval==1;
+    end
+
+//TICLR
+always @(posedge stable_clk)
+    if(~rstn||software_we && waddr==`CSR_TICLR && we[`TICLR_CLR] && wdata[`TICLR_CLR])
+        timer_int <= 0;
+    else if(time_out)
+        timer_int <= 1;
+
+//CTAG
+always @(posedge clk)
+    if(~rstn) begin
+        csr_ctag <= 0;
+    end else if(software_we&&waddr==`CSR_CTAG) begin
+        if(we[ 0]) csr_ctag[ 0]<=wdata[ 0];
+        if(we[ 1]) csr_ctag[ 1]<=wdata[ 1];
+        if(we[ 2]) csr_ctag[ 2]<=wdata[ 2];
+        if(we[ 3]) csr_ctag[ 3]<=wdata[ 3];
+        if(we[ 4]) csr_ctag[ 4]<=wdata[ 4];
+        if(we[ 5]) csr_ctag[ 5]<=wdata[ 5];
+        if(we[ 6]) csr_ctag[ 6]<=wdata[ 6];
+        if(we[ 7]) csr_ctag[ 7]<=wdata[ 7];
+        if(we[ 8]) csr_ctag[ 8]<=wdata[ 8];
+        if(we[ 9]) csr_ctag[ 9]<=wdata[ 9];
+        if(we[10]) csr_ctag[10]<=wdata[10];
+        if(we[11]) csr_ctag[11]<=wdata[11];
+        if(we[12]) csr_ctag[12]<=wdata[12];
+        if(we[13]) csr_ctag[13]<=wdata[13];
+        if(we[14]) csr_ctag[14]<=wdata[14];
+        if(we[15]) csr_ctag[15]<=wdata[15];
+        if(we[16]) csr_ctag[16]<=wdata[16];
+        if(we[17]) csr_ctag[17]<=wdata[17];
+        if(we[18]) csr_ctag[18]<=wdata[18];
+        if(we[19]) csr_ctag[19]<=wdata[19];
+        if(we[20]) csr_ctag[20]<=wdata[20];
+        if(we[21]) csr_ctag[21]<=wdata[21];
+        if(we[22]) csr_ctag[22]<=wdata[22];
+        if(we[23]) csr_ctag[23]<=wdata[23];
+        if(we[24]) csr_ctag[24]<=wdata[24];
+        if(we[25]) csr_ctag[25]<=wdata[25];
+        if(we[26]) csr_ctag[26]<=wdata[26];
+        if(we[27]) csr_ctag[27]<=wdata[27];
+        if(we[28]) csr_ctag[28]<=wdata[28];
+        if(we[29]) csr_ctag[29]<=wdata[29];
+        if(we[30]) csr_ctag[30]<=wdata[30];
+        if(we[31]) csr_ctag[31]<=wdata[31];
+    end
 ///////////////////////////////////////
 //CSR read
 always @* begin
-    case(waddr)
-    `CSR_CRMD     : rdata = csr_crmd     ;
-    `CSR_PRMD     : rdata = csr_prmd     ;
-    `CSR_ECFG     : rdata = csr_ecfg     ;
-    `CSR_ESTAT    : rdata = csr_estat    ;
-    `CSR_ERA      : rdata = csr_era      ;
-    `CSR_BADV     : rdata = csr_badv     ;
-    `CSR_EENTRY   : rdata = csr_eentry   ;
-    `CSR_SAVE0    : rdata = csr_save0    ;
-    `CSR_SAVE1    : rdata = csr_save1    ;
-    `CSR_SAVE2    : rdata = csr_save2    ;
-    `CSR_SAVE3    : rdata = csr_save3    ;
-    // `CSR_TID      : rdata = csr_tid      ;
-    // `CSR_TCFG     : rdata = csr_tcfg     ;
-    // `CSR_TVAL     : rdata = csr_tval     ;
-    // `CSR_TICLR    : rdata = csr_ticlr    ;
-    // `CSR_CTAG     : rdata = csr_ctag     ;
-    default       : rdata = 0            ;
+    case(raddr_a)
+    `CSR_CRMD     : rdata_a = csr_crmd     ;
+    `CSR_PRMD     : rdata_a = csr_prmd     ;
+    `CSR_ECFG     : rdata_a = csr_ecfg     ;
+    `CSR_ESTAT    : rdata_a = csr_estat    ;
+    `CSR_ERA      : rdata_a = csr_era      ;
+    `CSR_BADV     : rdata_a = csr_badv     ;
+    `CSR_EENTRY   : rdata_a = csr_eentry   ;
+    `CSR_SAVE0    : rdata_a = csr_save0    ;
+    `CSR_SAVE1    : rdata_a = csr_save1    ;
+    `CSR_SAVE2    : rdata_a = csr_save2    ;
+    `CSR_SAVE3    : rdata_a = csr_save3    ;
+    `CSR_TID      : rdata_a = csr_tid      ;
+    `CSR_TCFG     : rdata_a = csr_tcfg     ;
+    `CSR_TVAL     : rdata_a = csr_tval     ;
+    `CSR_TICLR    : rdata_a = csr_ticlr    ;
+    `CSR_CTAG     : rdata_a = csr_ctag     ;
+    default       : rdata_a = 0            ;
+    endcase
+
+    case(raddr_b)
+    `CSR_CRMD     : rdata_b = csr_crmd     ;
+    `CSR_PRMD     : rdata_b = csr_prmd     ;
+    `CSR_ECFG     : rdata_b = csr_ecfg     ;
+    `CSR_ESTAT    : rdata_b = csr_estat    ;
+    `CSR_ERA      : rdata_b = csr_era      ;
+    `CSR_BADV     : rdata_b = csr_badv     ;
+    `CSR_EENTRY   : rdata_b = csr_eentry   ;
+    `CSR_SAVE0    : rdata_b = csr_save0    ;
+    `CSR_SAVE1    : rdata_b = csr_save1    ;
+    `CSR_SAVE2    : rdata_b = csr_save2    ;
+    `CSR_SAVE3    : rdata_b = csr_save3    ;
+    `CSR_TID      : rdata_b = csr_tid      ;
+    `CSR_TCFG     : rdata_b = csr_tcfg     ;
+    `CSR_TVAL     : rdata_b = csr_tval     ;
+    `CSR_TICLR    : rdata_b = csr_ticlr    ;
+    `CSR_CTAG     : rdata_b = csr_ctag     ;
+    default       : rdata_b = 0            ;
     endcase
 end
 assign plv = crmd_plv;
 assign era_out = csr_era;
 assign eentry = csr_eentry;
-assign has_interrupt_cpu  = crmd_ie&&(ecfg_lie&csr_estat[`ESTAT_IS])!=0;
-assign has_interrupt_idle = csr_estat[`ESTAT_IS]!=0;
+assign has_interrupt_cpu  = crmd_ie & (|(ecfg_lie&csr_estat[`ESTAT_IS]));
+assign has_interrupt_idle = |csr_estat[`ESTAT_IS];
 assign translate_mode = {crmd_pg,crmd_da};
 assign direct_i_mat = crmd_datf;
 assign direct_d_mat = crmd_datm;
