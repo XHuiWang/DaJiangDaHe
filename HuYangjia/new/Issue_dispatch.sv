19e38329e2622129a98047e0f1428d772be890f4
Huyangjia <Huyangjia@mail.ustc.edu.cn>
2024-08-01 15:54:34 +0800

    small bug

--- HuYangjia/new/Issue_dispatch.sv
+++ HuYangjia/new/Issue_dispatch.sv
@@ -42,6 +42,7 @@ module Issue_dispatch(
     logic [ 0: 0] double_BR;
     logic [ 0: 0] double_LDSW;
     logic [ 0: 0] LDSW_Mul_Div_csr_rdcnt_plus_any;
+    logic [ 0: 0] BR_LDST_prob;
     // logic [ 0: 0] any_plus_LDSW;
     logic [ 0: 0] RAW_exist;
     logic [ 1: 0] ld_exist;
@@ -158,7 +159,7 @@ module Issue_dispatch(
                             o_set2.o_valid = 1'b0;
                             o_usingNUM = 2'd1;
                         end
-                        else if(double_BR || RAW_exist) begin
+                        else if(double_BR | RAW_exist | BR_LDST_prob) begin
                             o_set1.o_valid = 1'b1;
                             o_set2.o_valid = 1'b0;
                             o_usingNUM = 2'd1;
@@ -180,14 +181,17 @@ module Issue_dispatch(
     assign rdcnt_exist[1] = (o_usingNUM != 2'd0) & o_set1.inst_type[6];
     assign rdcnt_exist[0] = (o_usingNUM == 2'd2) & o_set2.inst_type[6];
     
+    // &is_valid <==> is_valid == 2'b11 
 
 
-    assign double_BR     = (is_valid == 2'b11) ? ( (|(i_set1.br_type)) && (|(i_set2.br_type))) : 0; // 同时为BR，两个都不是全0
-    assign LDSW_Mul_Div_csr_rdcnt_plus_any = (is_valid == 2'b11) ? (i_set1.inst_type != 10'h001) : 0; // 前LDSW，Div， Mul，3csr, ertn后任意
+    assign double_BR     = (&is_valid) ? ( (|(i_set1.br_type)) && (|(i_set2.br_type))) : 0; // 同时为BR，两个都不是全0
+    assign LDSW_Mul_Div_csr_rdcnt_plus_any = (&is_valid) ? (i_set1.inst_type != 10'h001) : 0; // 前LDSW，Div， Mul，3csr, ertn后任意
     
 
-    assign double_LDSW   = (is_valid == 2'b11) ? ( ~((i_set1.ldst_type[3]) | (i_set2.ldst_type[3])) ) : 0; // 同时为LDSW，最高位都是0
-    assign RAW_exist     = (is_valid == 2'b11 && i_set1.rf_rd != 0) ? ( (i_set1.rf_rd == i_set2.rf_raddr1) || (i_set1.rf_rd == i_set2.rf_raddr2) ) : 0; // 前rd=后rf,且rd = 0
+    assign double_LDSW   = (&is_valid ) ? ( ~((i_set1.ldst_type[3]) | (i_set2.ldst_type[3])) ) : 0; // 同时为LDSW，最高位都是0
+    assign RAW_exist     = (&is_valid  && i_set1.rf_rd != 0) ? ( (i_set1.rf_rd == i_set2.rf_raddr1) || (i_set1.rf_rd == i_set2.rf_raddr2) ) : 0; // 前rd=后rf,且rd = 0
+    assign BR_LDST_prob  = (&is_valid ) ? ((~i_set2.ldst_type[3]) & (|(i_set1.br_type))) : 0; // 前BR后LDSW
+
 
     assign lock_in_1     = (is_valid[1] & last_ld) ? (rf_waddr == i_set1.rf_raddr1) || (rf_waddr == i_set1.rf_raddr2) : 0; // LW和RDCNT与A指令互锁
     assign lock_in_2     = (is_valid[0] & last_ld) ? (rf_waddr == i_set2.rf_raddr1) || (rf_waddr == i_set2.rf_raddr2) : 0; // LW和RDCNT与B指令互锁
