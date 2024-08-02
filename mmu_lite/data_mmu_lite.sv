d5859310eed360e6148b2a10b7c38ebe6b0a256c
Wang Xianghui <142162719+XHuiWang@users.noreply.github.com>
2024-08-02 11:28:15 +0800

    Add files via upload
    update mmu_lite

--- mmu_lite/data_mmu_lite.sv
+++ mmu_lite/data_mmu_lite.sv
@@ -6,39 +6,43 @@
 
 module data_mmu_lite(
 
-input        [31:0]   addr,
-input        [31:0]   CRMD,
-input        [31:0]   DMW0,
-input        [31:0]   DMW1,
+input       [31:0]   addr,
+input        [1:0]    plv,
+input        [1:0]    translate_mode, //01: direct, 10: paged
+input        [1:0]    direct_d_mat,
+input                 dmw0_plv0,
+input                 dmw0_plv3,
+input        [1:0]    dmw0_mat,
+input        [2:0]    dmw0_vseg,dmw0_pseg,
+input                 dmw1_plv0,
+input                 dmw1_plv3,
+input        [1:0]    dmw1_mat,
+input        [2:0]    dmw1_vseg,dmw1_pseg,
+
 
 output logic [31:0]   paddr,
 output logic          uncache
 );
 
+logic  dmw0_en;
+assign dmw0_en =  (plv == 2'b11 && dmw0_plv3) || (plv == 2'b00 && dmw0_plv0);
+logic  dmw1_en;
+assign dmw1_en =  (plv == 2'b11 && dmw1_plv3) || (plv == 2'b00 && dmw1_plv0);
 
 
-//直接翻译模式 DA=1 PG=0
-always@(*) begin
-    if(CRMD[3] == 1) begin
-        paddr   = addr;
-        uncache = ~CRMD[7];
+always @(*) begin
+    if(translate_mode == 2'b10 && dmw0_en && addr[31:29] == dmw0_vseg) begin
+        paddr   = {dmw0_pseg, addr[28:0]};
+        uncache = ~dmw0_mat[0];
     end
-    
+    else if(translate_mode == 2'b10 && dmw1_en && addr[31:29] == dmw1_vseg) begin
+        paddr   = {dmw1_pseg, addr[28:0]};
+        uncache = ~dmw1_mat[0];
+    end
     else begin
-        if(addr[31:29] == DMW0[31:29]) begin
-            paddr   = {DMW0[27:25], addr[28:0]};
-            uncache = ~DMW0[4];  
-        end
-        else if(addr[31:29] == DMW1[31:29]) begin
-            paddr   = {DMW1[27:25], addr[28:0]};
-            uncache = ~DMW1[4];  
-        end
-        else begin
-            paddr   = addr;
-            uncache = ~CRMD[7];
-        end
+        paddr   = addr;
+        uncache = ~direct_d_mat[0];
     end
-
 end
 
 
