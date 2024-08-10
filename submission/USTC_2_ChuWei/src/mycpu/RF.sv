4778a2ba0f8d89db58ee28191706d80a37b26914
Huyangjia <Huyangjia@mail.ustc.edu.cn>
2024-08-10 21:10:17 +0800

    support for diffset.RF

--- submission/USTC_2_ChuWei/src/mycpu/RF.sv
+++ submission/USTC_2_ChuWei/src/mycpu/RF.sv
@@ -29,7 +29,16 @@ module RF #(
     output  reg [DATA_WIDTH -1:0]   rdata_a1  , rdata_a2  , rdata_b1  , rdata_b2,   //读数据
     input       [ADDR_WIDTH -1:0]   waddr_a, waddr_b,                       //写地址
     input       [DATA_WIDTH -1:0]   wdata_a, wdata_b,                       //写数据
-    input                           we_a,we_b                               //写使能
+    input                           we_a,we_b,                              //写使能
+    output logic [ADDR_WIDTH -1:0]  regs1, regs2, regs3, regs4,
+                                    regs5, regs6, regs7, regs8,
+                                    regs9, regs10, regs11, regs12,
+                                    regs13, regs14, regs15, regs16,
+                                    regs17, regs18, regs19, regs20,
+                                    regs21, regs22, regs23, regs24,
+                                    regs25, regs26, regs27, regs28,
+                                    regs29, regs30, regs31                       //寄存器堆
+
 );
     reg [DATA_WIDTH -1:0]  rf [0:(1<<ADDR_WIDTH)-1];    //寄存器堆
     //异步读
@@ -45,5 +54,39 @@ module RF #(
     always@ (posedge clk) begin
         if (we_a && (!we_b || waddr_a!=waddr_b))     rf[waddr_a] <= wdata_a;
         if (we_b)                                    rf[waddr_b] <= wdata_b;
+    end
+
+    always @(*) begin
+        regs1 = rf[1];
+        regs2 = rf[2];
+        regs3 = rf[3];
+        regs4 = rf[4];
+        regs5 = rf[5];
+        regs6 = rf[6];
+        regs7 = rf[7];
+        regs8 = rf[8];
+        regs9 = rf[9];
+        regs10 = rf[10];
+        regs11 = rf[11];
+        regs12 = rf[12];
+        regs13 = rf[13];
+        regs14 = rf[14];
+        regs15 = rf[15];
+        regs16 = rf[16];
+        regs17 = rf[17];
+        regs18 = rf[18];
+        regs19 = rf[19];
+        regs20 = rf[20];
+        regs21 = rf[21];
+        regs22 = rf[22];
+        regs23 = rf[23];
+        regs24 = rf[24];
+        regs25 = rf[25];
+        regs26 = rf[26];
+        regs27 = rf[27];
+        regs28 = rf[28];
+        regs29 = rf[29];
+        regs30 = rf[30];
+        regs31 = rf[31];
     end
 endmodule
--- submission/USTC_2_ChuWei/src/mycpu/mycpu_top.sv
+++ submission/USTC_2_ChuWei/src/mycpu/mycpu_top.sv
@@ -422,10 +422,12 @@ module mycpu_top(
     assign flush_of_ALL = MEM_br | WB_era_en | WB_eentry_en | WB_flush_csr;
     
   
-    // assign pc_predict = ~(|pred0_br_type) ? {pred1_br_target, 2'b00} : {pred0_br_target, 2'b00};
-    assign pc_predict = ~(is_valid) ? pc_IF1 :{predict_br_target,2'b00};
+    // signs for difftest
+    logic [31: 0] regs[31: 0];
 
     
+    // assign pc_predict = ~(|pred0_br_type) ? {pred1_br_target, 2'b00} : {pred0_br_target, 2'b00};
+    assign pc_predict = ~(is_valid) ? pc_IF1 :{predict_br_target,2'b00};
     IF1  IF1_inst (
         .clk(clk),
         .rstn(rstn),
@@ -711,9 +713,40 @@ module mycpu_top(
         .wdata_a(WB_rf_wdata_a),
         .wdata_b(WB_rf_wdata_b),
         .we_a(WB_rf_we_a),
-        .we_b(WB_rf_we_b)
+        .we_b(WB_rf_we_b),
+        .regs1(regs[1]),
+        .regs2(regs[2]),
+        .regs3(regs[3]),
+        .regs4(regs[4]),
+        .regs5(regs[5]),
+        .regs6(regs[6]),
+        .regs7(regs[7]),
+        .regs8(regs[8]),
+        .regs9(regs[9]),
+        .regs10(regs[10]),
+        .regs11(regs[11]),
+        .regs12(regs[12]),
+        .regs13(regs[13]),
+        .regs14(regs[14]),
+        .regs15(regs[15]),
+        .regs16(regs[16]),
+        .regs17(regs[17]),
+        .regs18(regs[18]),
+        .regs19(regs[19]),
+        .regs20(regs[20]),
+        .regs21(regs[21]),
+        .regs22(regs[22]),
+        .regs23(regs[23]),
+        .regs24(regs[24]),
+        .regs25(regs[25]),
+        .regs26(regs[26]),
+        .regs27(regs[27]),
+        .regs28(regs[28]),
+        .regs29(regs[29]),
+        .regs30(regs[30]),
+        .regs31(regs[31])
     );
-
+   
     CSR  CSR_inst (
         .clk(clk),
         .stable_clk(stable_clk),
@@ -1087,6 +1120,41 @@ module mycpu_top(
         .s_axi_rvalid   ({ i_axi_rvalid   ,  d_axi_rvalid   }),
         .s_axi_rready   ({ i_axi_rready   ,  d_axi_rready   })
     );
-
+    DifftestGRegState DifftestGRegState(
+        .clock              (aclk       ),
+        .coreid             (0          ),
+        .gpr_0              (0          ),
+        .gpr_1              (regs[1]    ),
+        .gpr_2              (regs[2]    ),
+        .gpr_3              (regs[3]    ),
+        .gpr_4              (regs[4]    ),
+        .gpr_5              (regs[5]    ),
+        .gpr_6              (regs[6]    ),
+        .gpr_7              (regs[7]    ),
+        .gpr_8              (regs[8]    ),
+        .gpr_9              (regs[9]    ),
+        .gpr_10             (regs[10]   ),
+        .gpr_11             (regs[11]   ),
+        .gpr_12             (regs[12]   ),
+        .gpr_13             (regs[13]   ),
+        .gpr_14             (regs[14]   ),
+        .gpr_15             (regs[15]   ),
+        .gpr_16             (regs[16]   ),
+        .gpr_17             (regs[17]   ),
+        .gpr_18             (regs[18]   ),
+        .gpr_19             (regs[19]   ),
+        .gpr_20             (regs[20]   ),
+        .gpr_21             (regs[21]   ),
+        .gpr_22             (regs[22]   ),
+        .gpr_23             (regs[23]   ),
+        .gpr_24             (regs[24]   ),
+        .gpr_25             (regs[25]   ),
+        .gpr_26             (regs[26]   ),
+        .gpr_27             (regs[27]   ),
+        .gpr_28             (regs[28]   ),
+        .gpr_29             (regs[29]   ),
+        .gpr_30             (regs[30]   ),
+        .gpr_31             (regs[31]   )
+    );
 
 endmodule
